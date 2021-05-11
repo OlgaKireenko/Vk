884b6911e54a9018eeefebfa6f455220490edb98
Kireenko Olga <kireenko86.osil@mail.ru>
2021-05-11 13:28:31 +0500

    add code

---
+++ src/main/java/domain/CommentsInfo.java
@@ -0,0 +1,6 @@
+package domain;
+
+public class CommentsInfo {
+    int count;
+    boolean canreply;
+}
---
+++ src/main/java/domain/Post.java
@@ -0,0 +1,13 @@
+package domain;
+
+public class Post {
+    private int postID;
+    private String authorID;
+    private String textBody;
+    private String infoComments;
+    private int countLike;
+    private int countShare;
+    private int countBrowse;
+    private int date;
+    private boolean privateStatus; // who can browse only friends or not
+}
