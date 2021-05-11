1dcbacb808efd9dc2d176780cb97a46faeea0619
Kireenko Olga <kireenko86.osil@mail.ru>
2021-05-11 15:08:06 +0500

    add manager

---
+++ src/main/java/manager/PostManager.java
@@ -0,0 +1,14 @@
+package manager;
+
+import domain.Post;
+
+public class PostManager {  //создаю класс
+    private PostManager postManager; // создаю объект из класса
+    public Post[] search[] { // должен быть метод search
+        // some logic
+        return null;
+    }
+    public void delete {  //метод delete
+
+    }
+}
